8c1a3fd58212dc4a4da775ab38251a982b2a09ae
Wesner M <wesnerm@gmail.com>
2023-03-26 14:20:54 -0400

    Simple update in routes

--- server.js
+++ server.js
@@ -1,40 +1,45 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const router = express.Router();
+const methodOverride = require("method-override");
 
-const dataController = require("./controllers/data");
+// Models - Database stuff
+const pokemons = require("./models/budjets.js");
 
-const models = require("./models/data");
-
-const data = models.data;
-
+//Middleware
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 app.use((req, res, next) => {
-  console.log("Budgtr");
+  console.log("Middleware working");
   next();
 });
 
-// Routes
-// Hungry for more to create my own API, and APIs always should be in JSON
-app.get("/api", (req, res) => {
-  res.json({
-    models,
-    status: 200,
-  });
-});
-
+//INDEX ROUTE
 app.get("/", (req, res) => {
-  res.render("budgets.ejs");
+  res.render("budgets", { budgets }); // or { budgets: budjets }
 });
 
-
-app.use("/budgets", budgetsController);
-
-app.get("/*", (req, res) => {
-  res.render("404.ejs");
+//SHOW ROUTE
+app.get("/budgets/:index", (req, res) => {
+  let budjet = budgets[req.params.id];
+  res.render("budgets.js", { show: budgets });
+  console.log(show);
 });
+//NEW ROUTE
+app.get("/budgets/new/"),
+  (req, res) => {
+    res.render("new.ejs", { budgets });
+  };
+
+//CREATE ROUTE
+app.post("/budgets/"),
+  (req, res) => {
+    res.render("budgets", { budgets });
+  };
 
 app.listen(port, () => {
-  console.log(`🏝️ Server is listening to PORT ${port} `);
+  console.log(`🏝️ Server is listening to PORT ${port}`);
 });
